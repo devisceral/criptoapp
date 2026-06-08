226191b62aef76aaa258dcf951fe347abb8968f0
devisceral <develmendes@gmail.com>
2026-06-08 17:19:12 -0300

    feat: adiciona form de pesquisa de criptomoeda

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,18 @@
+import { BsSearch } from "react-icons/bs"
+import styles from "./home.module.css"
+
 export function Home(){
     return(
-        <div>
-            <h1>Home</h1>
-        </div>
+        <main className={styles.container}>
+            <form className={styles.form}>
+                <input 
+                    type="text"
+                    placeholder="Digite o nome da moeda"
+                />
+                <button type="submit">
+                    <BsSearch size={30} color="#FFF" />
+                </button>
+            </form>
+        </main>
     )
 }
