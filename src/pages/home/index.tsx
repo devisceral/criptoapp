c3c99e5684a982185db7913760b477700ce0764b
devisceral <develmendes@gmail.com>
2026-06-08 17:29:18 -0300

    feat: criação de items estáticos da tabela

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import { BsSearch } from "react-icons/bs"
 import styles from "./home.module.css"
+import { Link } from "react-router"
 
 export function Home(){
     return(
@@ -13,6 +14,42 @@ export function Home(){
                     <BsSearch size={30} color="#FFF" />
                 </button>
             </form>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th scope="col">Moeda</th>
+                        <th scope="col">Valor de mercado</th>
+                        <th scope="col">Preço</th>
+                        <th scope="col">Volume</th>
+                        <th scope="col">Mudança 24h</th>
+                    </tr>
+                </thead>
+
+                <tbody id="tbody">
+                    <tr className={styles.tr}>
+                        <td className={styles.tdLabel} data-label="Moeda">
+                            <div className="name">
+                                <Link to={"/detail/bitcoin"}>
+                                    <span>Bitcoin</span> | BTC
+                                </Link>
+                            </div>
+                        </td>
+                        <td className={styles.tdLabel} data-label="Valor de mercado">
+                            $1,000,000
+                        </td>
+                        <td className={styles.tdLabel} data-label="Preço">
+                            $50,000
+                        </td>
+                        <td className={styles.tdLabel} data-label="Volume">
+                            $10,000,000
+                        </td>
+                        <td className={styles.tdLabel} data-label="Mudança 24h">
+                            <span>+2.5%</span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </main>
     )
 }
